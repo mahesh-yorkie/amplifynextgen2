9df64a520cbd45ed53d3eb459422c64df87dcfc7
mahesh chomal <maheshchomal@york.ie>
2024-07-12 13:13:58 +0530

    changes triger

--- amplify/auth/custom-message/handler.ts
+++ amplify/auth/custom-message/handler.ts
@@ -1,14 +1,14 @@
 import type { CustomMessageTriggerHandler } from "aws-lambda";
 
 export const handler: CustomMessageTriggerHandler = async (event) => {
-
-	const enviroment = process.env.ENV;
+	console.log(event)
+	let ENVIROMENT = process.env.ENV;
 	let REDIRECTURL = "http://localhost:3007/";
-	if (enviroment == "dev") {
+	if (ENVIROMENT == "dev") {
 		//REDIRECTURL = "http://localhost:3007/auth/reset-password"
-	} else if (enviroment == "stage") {
+	} else if (ENVIROMENT == "stage") {
 		///REDIRECTURL = ""
-	} else if (enviroment == "prod") {
+	} else if (ENVIROMENT == "prod") {
 		//REDIRECTURL = ""
 	}
 
@@ -20,7 +20,6 @@ export const handler: CustomMessageTriggerHandler = async (event) => {
         REDIRECTURL = REDIRECTURL + "auth/reset-password";
 
         let redirectUrl = `${REDIRECTURL}/?username=${userName}`;
-        const resourcePrefix = process.env.RESOURCENAME.split("CustomMessage")[0];
         const payload = Buffer.from(
         JSON.stringify({
             userName,
@@ -31,7 +30,7 @@ export const handler: CustomMessageTriggerHandler = async (event) => {
         ).toString("base64");
 
         const url = `${redirectUrl}&data=${payload}&code=${codeParameter}`;
-        const message = `${process.env.EMAILMESSAGE}. \n ${url}`;
+        const message = `We received a request to reset your password for your [Your App Name] account. If you didn't make this request, please ignore this email.To reset your password, click the link below. \n ${url}`;
         event.response.smsMessage = message;
         event.response.emailSubject = process.env.EMAILSUBJECT;
         event.response.emailMessage = message;
@@ -40,14 +39,7 @@ export const handler: CustomMessageTriggerHandler = async (event) => {
     if (event.triggerSource === "CustomMessage_AdminCreateUser") {
         REDIRECTURL = REDIRECTURL + "auth/login";
         const url = `${REDIRECTURL}/?email=${event.request.userAttributes.email}&password=${event.request.codeParameter}`;
-        // console.log("url", url);
-        // const message = `Welcome to CurrentPools!\n Hi ${event.request.userAttributes.email},\n Thank you for signing up. Please click the link below to login:\n
-        //     ${url}`;
-        // event.response.smsMessage = message;
-        // event.response.emailSubject = message //"Welcome to CurrentPools";
-        // event.response.emailMessage = message;
-        // console.log("event.response", event.response);
-
+        
 
         const message = `<h5>Welcome to CurrentPools.</h5> Your email is ${event.request.usernameParameter}. Your temporary password is ${event.request.codeParameter} \n  <a href='${url}'>click to login</a>`;
         event.response.smsMessage = message;

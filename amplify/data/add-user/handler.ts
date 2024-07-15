903bc7422ed5a97394653209fa4e20c2d0d3f4e2
mahesh chomal <maheshchomal@york.ie>
2024-07-15 17:29:33 +0530

    create user

--- amplify/data/add-user/handler.ts
+++ amplify/data/add-user/handler.ts
@@ -1,47 +1,55 @@
-import type { Schema } from "../resource"
-import { env } from "$amplify/env/add-user"
+import type { Schema } from "../resource";
+import { env } from "$amplify/env/add-user";
 import {
-  AdminCreateUserCommand
+  AdminCreateUserCommand,
   AdminAddUserToGroupCommand,
   CognitoIdentityProviderClient,
-} from "@aws-sdk/client-cognito-identity-provider"
+} from "@aws-sdk/client-cognito-identity-provider";
 
-type Handler = Schema["addAdminUser"]["functionHandler"]
-const client = new CognitoIdentityProviderClient()
+type Handler = Schema["addAdminUser"]["functionHandler"];
+const client = new CognitoIdentityProviderClient({ region: env.AWS_REGION });
 
 export const handler: Handler = async (event) => {
-  console.log("testing")
-  const { email, groupList } = event.arguments
+  console.log("testing");
+  const { email, groupList } = event.arguments;
 
   const argAdminCreateUser = {
-      UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
-      Username: email,
-      TemporaryPassword: `A${Math.random().toString(36).slice(2)}@0`,
-      DesiredDeliveryMediums: ["EMAIL"],
-      UserAttributes: [
-          {
-              Name: "email",
-              Value: email
-          },
-          {
-              Name: "email_verified",
-              Value: "true"
-          }
-      ]
+    UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
+    Username: email,
+    TemporaryPassword: `A${Math.random().toString(36).slice(2)}@0`,
+    DesiredDeliveryMediums: ["EMAIL"],
+    UserAttributes: [
+      {
+        Name: "email",
+        Value: email,
+      },
+      {
+        Name: "email_verified",
+        Value: "true",
+      },
+    ],
+  };
+
+  const createUserCommand = new AdminCreateUserCommand(argAdminCreateUser);
+  const response: { user?: any; groups?: any[] } = {};
+
+  try {
+    response.user = await client.send(createUserCommand);
+
+    response.groups = [];
+    for (const groupToAdd of groupList) {
+      const addUserToGroupCommand = new AdminAddUserToGroupCommand({
+        Username: email,
+        GroupName: groupToAdd,
+        UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
+      });
+      const groupResponse = await client.send(addUserToGroupCommand);
+      response.groups.push(groupResponse);
+    }
+  } catch (error) {
+    console.error("Error creating user or adding to groups:", error);
+    throw error;
   }
 
-  const command = new AdminCreateUserCommand(input);
-  let response["user"] = await client.send(command);
-
-  for (const groupToAdd of groupList) {
-    const command = new AdminAddUserToGroupCommand({
-      Username: email,
-      GroupName: groupToAdd,
-      UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID,
-    })
-    response['group'] = await client.send(command)
-
-  }
-  
-  return response
-}
+  return response;
+};

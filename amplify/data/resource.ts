603fb22e3aed6fe0a91868586e959d00fddb417c
mahesh chomal <maheshchomal@york.ie>
2024-07-15 11:59:43 +0530

    schme changes

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,39 +1,199 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-      email:a.string(),
-      name:a.string(),
-      phone:a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
     Company: a.model({
-      id: a.id(),
-      logo: a.string(),
-      name: a.string(),
-      description: a.string(),
-      address: a.string(),
-      phone: a.string(),
-      email: a.string(),
-      website: a.string(),
-      businessLicense: a.string(),
-      termsAndConditions: a.string(),
-      //Users: a.hasMany('User', 'companyId'),//[User] @hasMany
-      //PoolService :a.hasMany('PoolService', 'companyId'),
-      //PoolServiceLocation:a.hasMany('PoolServiceLocation', 'companyId'),
-      isActive: a.boolean().default(true),
-      activationDate: a.time()
-  }).authorization((allow) => [
-    allow.authenticated(),
-  ]),
+        id: a.id().required(),
+        logo: a.string(),
+        name: a.string().required(),
+        description: a.string(),
+        address: a.string(),
+        phone: a.string(),
+        email: a.string(),
+        website: a.string(),
+        businessLicense: a.string(),
+        termsAndConditions: a.string(),
+        Users: a.hasMany('User', 'companyId'),//[User] @hasMany
+        PoolService :a.hasMany('PoolService', 'companyId'),
+        PoolServiceLocation:a.hasMany('PoolServiceLocation', 'companyId'),
+        isActive: a.boolean().default(true),
+        activationDate: a.time()
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+   
+    PlanFeature:a.model({
+      id:a.id().required(),
+      name:a.string().required(),
+      description:a.string(),
+      slug:a.string().required(),
+      subscription :a.hasMany('PlanFeatureSubscriptionPlan','PlanFeatureId')
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    SubscriptionPlan:a.model({
+      id:a.id().required(),
+      title:a.string().required(),
+      description:a.string().required(),
+      billingCycle:a.enum(['MONTHLY','QUARTERLY','YEARLY']),
+      amount:a.float().required(),
+      type: a.enum(['BASIC','ESSENTIAL','PREMIUM']),
+      isActive:a.boolean().default(true),
+      features:a.hasMany('PlanFeatureSubscriptionPlan', 'SubscriptionPlanId') 
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    PlanFeatureSubscriptionPlan:a.model({
+      id:a.id().required(),
+      SubscriptionPlanId:a.id().required(),
+      PlanFeatureId:a.id().required(),
+      subscriptionplan:a.belongsTo('SubscriptionPlan','SubscriptionPlanId'),
+      planfeature:a.belongsTo('PlanFeature','PlanFeatureId')
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    User:a.model({
+      id:a.id().required(),
+      companyID:a.id().required(),
+      company:a.belongsTo('Company', 'companyID'),
+      name:a.string(),
+      email:a.required().string(),
+      phone:a.string(),
+      picture:a.string(),
+      address:a.customType({
+        lat: a.float(),
+        long: a.float(),
+      }),
+      userGroupList:a.string().array(),
+      subscriptionPlanID:a.id(),
+      subscriptionPlan:a.hasOne('SubscriptionPlan', 'subscriptionPlanID'),
+      subscriptionHistories:a.hasMany('UserSubscriptionHistory', 'userID'),
+      //requests:a.hasMany('Request', 'userID'),
+      //quotes:a.hasMany('Quote', 'userID'),
+      //notifications:a.hasMany('UserNotification', 'userID'),
+      cognitoUsername:a.string(),
+      cognitoUserStatus:a.enum(['CONFIRMED', 'RESET_REQUIRED']),
+      isActive:a.boolean().default(true),
+      activationDate:a.time()
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    UserSubscriptionHistory:a.model({
+      id:a.id().required(),
+      userID:a.id().required(),
+      user:a.belongsTo('User', 'userID'),
+      subscriptionPlanID:a.id().required(),
+      subscriptionPlan:a.belongsTo('SubscriptionPlan', 'subscriptionPlanID'),
+      amount:a.float().required(),
+      startDate:a.time(),
+      endDate:a.time()
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    UserNotification:a.model({
+      id:a.id().required(),
+      title:a.string(),
+      description:a.string(),
+      type:a.enum(['BROADCAST', 'CONTRACT_SIGNED']),
+      userID:a.id().required(),
+      user:a.belongsTo('User', 'userID'),
+      isRead:a.boolean().default(false),
+      readAt:a.time(),
+      createdBy:a.id()
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    Country:a.model({
+      id:a.id().required(),
+      name:a.string().required(),
+      state:a.hasMany('State', 'countryID'),
+      PoolServiceLocation:a.hasMany('PoolServiceLocation', 'countryID'),
+      isActive:a.boolean().default(true)
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    State:a.model({
+      id:a.id().required(),
+      name:a.string().required(),
+      countryID:a.id().required(),
+      country:a.belongsTo('Country', 'countryID'),
+      city:a.hasMany('City', 'stateID'),
+      PoolServiceLocation:a.hasMany('PoolServiceLocation', 'stateID'),
+      isActive:a.boolean().default(true)
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    City:a.model({
+      id:a.id().required(),
+      name:a.string().required(),
+      stateID:a.id().required(),
+      state:a.belongsTo('State', 'stateID'),
+      PoolServiceLocation:a.hasMany('PoolServiceLocation', 'cityID'),
+      isActive:a.boolean().default(true)
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    PoolService:a.model({
+      id:a.id().required(),
+      companyID:a.id().required(),
+      company:a.belongsTo('Company', 'companyID'),
+      name:a.string(),
+      description:a.string(),
+      price:a.float(),
+      isActive:a.boolean().default(true)
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    PoolServiceLocation:a.model({
+      id:a.id().required(),
+      companyID:a.id().required(),
+      company:a.belongsTo('Company', 'companyID'),
+      countryID:a.id().required(),
+      country:a.belongsTo('Country', 'countryID'),
+      stateID:a.id().required(),
+      state:a.belongsTo('State', 'stateID'),
+      cityID:a.id().required(),
+      city:a.belongsTo('City', 'cityID'),
+      isActive:a.boolean().default(true)
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    HelpSupport:a.model({
+      id:a.id().required(),
+      title:a.string().required(),
+      description:a.string().required(),
+      isActive:a.boolean().default(true)
+    }).authorization((allow) => [
+    
+      allow.authenticated(),
+    ]),
+    Request:a.model({
+      id:a.id().required(),
+      companyID:a.id().required(),
+      company:a.belongsTo('Company', 'companyID'),
+      customerID:a.id().required(),
+      customer:a.belongsTo('User', 'customerID'),
+      title:a.string(),
+      address:a.string(),
+      description:a.string(),
+      preferredTime:a.enum(['ANYTIME', 'MORNING', 'AFTERNOON', 'EVENING']),
+      poolServiceList:a.string().array(),
+      stage:a.enum(['REQUEST', 'QUOTE', 'JOB', 'COMPLETED']),
+      quote:a.hasOne('Quote', 'requestID'),
+      isActive:a.boolean().default(true)
+    })
+    
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -41,7 +201,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
 

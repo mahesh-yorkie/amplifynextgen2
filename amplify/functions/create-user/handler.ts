68975524f171874e2e8419af88d7a26b3b7571f0
mahesh chomal <maheshchomal@york.ie>
2024-07-16 18:23:11 +0530

    get branch name

--- amplify/functions/create-user/handler.ts
+++ amplify/functions/create-user/handler.ts
@@ -2,5 +2,5 @@ import type { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event, context) => {
   // your function code goes here
-  return 'Hello, create user!' + process.env.AWS_BRANCH;
+  return 'Hello, create user!' + process.env.AMPLIFY_BRANCH;
 };

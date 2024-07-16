a5a9a2015cab432819353989911c0725c1d71991
mahesh chomal <maheshchomal@york.ie>
2024-07-16 18:40:20 +0530

    return string

--- amplify/functions/create-user/handler.ts
+++ amplify/functions/create-user/handler.ts
@@ -3,9 +3,9 @@ import type { Handler } from 'aws-lambda';
 export const handler: Handler = async (event, context) => {
   // your function code goes here
 
-  return {
+  return JSON.stringify({
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello from Amplify!'+ process.env.AMPLIFY_BRANCH }),
-  };
+  });
   //return 'Hello, create user!' + process.env.AMPLIFY_BRANCH;
 };

f9922812609f66b04706e91ac0cc9b2309206c42
mahesh chomal <maheshchomal@york.ie>
2024-07-17 11:12:11 +0530

    env define

--- amplify/functions/create-user/resource.ts
+++ amplify/functions/create-user/resource.ts
@@ -1,12 +1,18 @@
 import { defineFunction ,secret} from '@aws-amplify/backend';
 
+interface Environment {
+  NAME: string;
+  API_ENDPOINT: string | undefined;
+}
+
+const environment: Environment = {
+  NAME: "World",
+  API_ENDPOINT: process.env.API_ENDPOINT
+};
 export const crearteUser = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'create-user',
   // optionally specify a path to your handler (defaults to "./handler.ts")
   entry: './handler.ts',
-  environment: {
-    NAME: "World",
-    API_ENDPOINT: process.env.ENV
-  }
+  environment: environment
 });

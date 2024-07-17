3331711e52b6ded7143ed77f3abc099fe259a82e
mahesh chomal <maheshchomal@york.ie>
2024-07-17 11:17:39 +0530

    typescript error

--- amplify/functions/create-user/resource.ts
+++ amplify/functions/create-user/resource.ts
@@ -1,18 +1,14 @@
 import { defineFunction ,secret} from '@aws-amplify/backend';
 
-interface Environment {
-  NAME: string;
-  API_ENDPOINT: string | undefined;
-}
 
-const environment: Environment = {
-  NAME: "World",
-  API_ENDPOINT: process.env.API_ENDPOINT
-};
+const API_ENDPOINT: string | undefined = process.env.ENV;
 export const crearteUser = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'create-user',
   // optionally specify a path to your handler (defaults to "./handler.ts")
   entry: './handler.ts',
-  environment: environment
+  environment: {
+    NAME: "World",
+    API_ENDPOINT:API_ENDPOINT
+  }
 });

9cd6a04afb02b251c0b4c3ca93f2da3103dbe209
mahesh chomal <maheshchomal@york.ie>
2024-07-17 13:09:59 +0530

    typoe script

--- amplify/functions/create-user/resource.ts
+++ amplify/functions/create-user/resource.ts
@@ -1,7 +1,9 @@
 import { defineFunction ,secret} from '@aws-amplify/backend';
 
 
-const ENV: string = process.env.ENV || 'dev';
+const API_ENDPOINT: string = process.env.ENV || 'default_endpoint';
+
+
 
 export const crearteUser = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
@@ -9,6 +11,6 @@ export const crearteUser = defineFunction({
   // optionally specify a path to your handler (defaults to "./handler.ts")
   entry: './handler.ts',
   environment:{
-    ENV:ENV
+    API_ENDPOINT:API_ENDPOINT
   }
 });
